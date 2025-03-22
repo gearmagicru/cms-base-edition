1cc4493fea564d6a1ace1304196da4dbaa484a67
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-03-22 19:27:08 +0300

    Таблицы используемые картой SQL запросов (раздел "insert")

---
+++ .install/gm.setup/queries/tables/demo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Таблицы используемые картой SQL запросов (раздел "insert").
+ * 
+ * Веб-приложения «GearMagic: Управление сайтом» (GM СMS).
+ * Редакции «Базовая» (GM CMS:Base).
+ * 
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+/** @var bool $isRu Текущая установка в русской локализации*/
+$isRu = $installer->isRu();
+
+return [
+];
