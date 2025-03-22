3811f30b6d09838567da2d1657c5da6974f1fb47
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-03-22 19:23:05 +0300

    Файл конфигурации менеджера шорткодов (shortcodes)

---
+++ config/.shortcodes.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Файл конфигурации менеджера шорткодов (shortcodes).
+ * 
+ * Здесь перечисляются основные шорткоды, которые используются в шаблонах 
+ * компонентов (backend, frontend).
+* 
+ * Компоненты, которые имеют шорткоды и добавленны через Панель управления "GM Panel", будут в файле 
+ * конфигурации ".shortcodes.so" (включая шорткоды перечисленные ниже).
+ * 
+ * Пример записи шорткода в параметре 'shortcodes':
+ * 1) короткая запись `['tagName' => 'moduleId']`, 
+ * результат: `module->tagNameShortcode(attributes)`;
+ * 2) полная запись `['tagName' => ['componentType' => 'componentId', 'func' => 'componentMethod']`,
+ * результат: `component->componentMethod(attributes)`.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    /** Шаблон метода компонента (модуль, расширение, виджет) возвращающий шорткод */
+    'patternFunc' => '%sShortcode',
+
+    /** Теги шорткодов */
+    'shortcodes' => []
+];
